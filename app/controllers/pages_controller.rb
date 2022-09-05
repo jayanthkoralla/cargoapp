2652f6c94ab2388aca008809262ba8859b412c1a
jayanth <jayanth.koralla@gmail.com>
2022-09-05 10:51:10 +0530

    Done upto customers module before adding booings

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,9 @@ class PagesController < ApplicationController
       # render :template => "drivers/index"
       redirect_to drivers_path
     end
+    if customer_signed_in?
+      # render :template => "customers/index"
+      redirect_to customers_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'customers/index'
   resources :routes
   get 'pages/index'
   get "/drivers", to: "drivers#index"
+  get "/customers", to: "customers#index"
   devise_for :drivers
   devise_for :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
